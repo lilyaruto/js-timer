ba64504071acd5dc488505b7c9a500b9ca443097
Yaruto <neo960211@gmail.com>
2023-10-31 16:54:05 +0100

    Created Timer class

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hi there!")
+class Timer {
+    constructor(duration, startBtn, pauseBtn) {
+        this.duration = duration;
+        this.startBtn = startBtn;
+        this.pauseBtn = pauseBtn;
+    }
+
+    start() {
+        console.log("start");
+    }
+}
+
+const durationInput = document.getElementById("#duration");
+const startBtn = document.getElementById("#start");
+const pauseBtn = document.getElementById("#pause");
+
+const timer = new Timer(durationInput, startBtn, pauseBtn);
