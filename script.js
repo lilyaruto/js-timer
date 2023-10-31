f0c0663a1d633427bcd8107968132698b33af005
Yaruto <neo960211@gmail.com>
2023-10-31 17:49:06 +0100

    Added start and pause methods

--- script.js
+++ script.js
@@ -3,15 +3,28 @@ class Timer {
         this.duration = duration;
         this.startBtn = startBtn;
         this.pauseBtn = pauseBtn;
+
+        this.startBtn.addEventListener("click", this.start);
+        this.pauseBtn.addEventListener("click", this.pause);
     }
 
-    start() {
+    start = () => {
         console.log("start");
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    pause = () => {
+        clearInterval(this.interval);
+        console.log("pause");
+    }
+
+    tick = () => {
+        console.log("tick");
     }
 }
 
-const durationInput = document.getElementById("#duration");
-const startBtn = document.getElementById("#start");
-const pauseBtn = document.getElementById("#pause");
+const durationInput = document.getElementById("duration");
+const startBtn = document.getElementById("start");
+const pauseBtn = document.getElementById("pause");
 
 const timer = new Timer(durationInput, startBtn, pauseBtn);
